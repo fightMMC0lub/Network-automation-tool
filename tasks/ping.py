85633528347b1decab1df25bb8025d94482c31a6
fouad <getshop463@gmail.com>
2025-05-03 16:42:03 +0100

    Update ping.py

--- tasks/ping.py
+++ tasks/ping.py
@@ -1,1 +1,9 @@
+import platform
+import subprocess
+
+def ping_host(host, logger):
+    command = ["ping", "-n" if platform.system() == "Windows" else "-c", "4", host]
+    logger.info(f"Pinging {host}...")
+    result = subprocess.run(command, capture_output=True, text=True)
+    logger.info(result.stdout)
 
