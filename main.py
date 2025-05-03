2e16b8879ad17372135e309cf74e3af61eec4cd6
fouad <getshop463@gmail.com>
2025-05-03 16:38:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from tasks import ping, ports, ip_config, traceroute
+from utils.logger import setup_logger
+
+logger = setup_logger()
+
+if __name__ == "__main__":
+    host = "8.8.8.8"
+    logger.info("Starting network automation tasks...")
+
+    ping.ping_host(host, logger)
+    ports.scan_ports(host, logger)
+    ip_config.get_ip_config(logger)
+    traceroute.run_traceroute(host, logger)
+
+    logger.info("Tasks completed.")
 
