a4b7d8d6e5240083d5e9397aa40f4029e81123b3
fouad <getshop463@gmail.com>
2025-05-03 16:43:44 +0100

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,12 @@
+import logging
+
+def setup_logger():
+    logger = logging.getLogger("NetworkAutomation")
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler("network_automation.log")
+    fh.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+    logger.addHandler(fh)
+
+    return logger
 
