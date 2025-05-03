1852fac3f737c77d10323b313524e97deaaff3e8
fouad <getshop463@gmail.com>
2025-05-03 16:43:05 +0100

    Update traceroute.py

--- tasks/traceroute.py
+++ tasks/traceroute.py
@@ -1,1 +1,9 @@
+import platform
+import subprocess
+
+def run_traceroute(host, logger):
+    cmd = "tracert" if platform.system() == "Windows" else "traceroute"
+    logger.info(f"Running traceroute on {host}...")
+    result = subprocess.run([cmd, host], capture_output=True, text=True)
+    logger.info(result.stdout)
 
